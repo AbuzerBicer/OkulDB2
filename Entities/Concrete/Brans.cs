8012d14389085f5ae604d10193fde79a1ce26ae1
AbuzerBicer <abicer@tpao.gov.tr>
2022-02-12 10:07:28 +0300

    Add project files.

---
+++ Entities/Concrete/Brans.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Brans
+    {
+        public byte BransID { get; set; }
+        public string BransAdi { get; set; }
+    }
+}
---
+++ Entities/Concrete/Gun.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Gun
+    {
+        public byte GunID { get; set; }
+        public string GunAdi { get; set; }
+    }
+}
